92f13226c2402712f14e2b88002d988916cf0336
Lars Walther <lwalther@novareto.de>
2023-12-15 15:46:10 +0000

    eigener typesense search über zopyx.typesense

--- src/edi/search/views/custom_search_view.py
+++ src/edi/search/views/custom_search_view.py
@@ -3,6 +3,8 @@
 from edi.search import _
 from Products.Five.browser import BrowserView
 
+from zopyx.typesense.api import API
+
 # from Products.Five.browser.pagetemplatefile import ViewPageTemplateFile
 
 
@@ -12,6 +14,22 @@ class CustomSearchView(BrowserView):
     # template = ViewPageTemplateFile('custom_search_view.pt')
 
     def __call__(self):
+        import pdb; pdb.set_trace()
+        if self.request.method == "POST":
+            pass
+        else:
+            pass
+
+        import pdb; pdb.set_trace()
         # Implement your own actions:
         self.msg = _(u'A small message')
         return self.index()
+
+    def search(self):
+        ts_api = API()
+        query = self.request.get("search")
+        if (query == ""):
+            return None
+        result = ts_api.search(query)
+        hits = result["found"]
+
